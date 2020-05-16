0431dda1180224df223c1cd8542f911b9e1ce3a7
bobtao-git <bobtao@gmail.com>
2020-05-16 16:26:10 -0500

    Update mysql1.sql

--- mysql1.sql
+++ mysql1.sql
@@ -5,65 +5,63 @@ drop database if exists db2;
 create database db2;
 
 show databases;   # This statement shows the list of databases 
-drop database if exists DBEx1;  # This statement drops/deletes the database named DBEx1   
-create database DBEx1;  # This statement creates the database named DBEx1 
-use DBEx1;  # Use the database DEBx1 we just created 
+drop database if exists DBEX1;  # This statement drops/deletes the database named DBEx1   
+create database DBEX1;  # This statement creates the database named DBEx1 
+use DBEX1;  # Use the database DEBx1 we just created 
 show tables;  # This statement shows the list of tables in the database DEBx1 
 
 drop table if exists employees;  
-drop table if exists Employees;  
-create table employees(LName varchar(20), IDN int, Salary decimal(20, 2), Deduction decimal(20, 2)); # Create the Employees table 
+drop table if exists employees;  
+create table employees(lname varchar(20), idn int, salary decimal(20, 2), deduction decimal(20, 2)); # Create the Employees table 
 insert into employees values ('Peter', 1, 1000, 10), ('Mary', 12, 2000, 0), ('Tony', 22, 4000, 32); /* Insert three rows into the Employees table */ 
 select * from employees; 
 
-create table HighEarners as select * from employees where Salary > 2500;
- select * from HighEarners;
+create table highearners as select * from employees where salary > 2500;
+ select * from highearners;
  
 
-drop table if exists IRAs; 
-create table IRAs(IDN int, 
-IRA_acct varchar(12), 
-Balance decimal(20, 2)); 
+drop table if exists iras; 
+create table iras(idn int, ira_acct varchar(12), balance decimal(20, 2)); 
 /* Create the IRAs table */ 
-describe IRAs; 
-insert into IRAs values(1, 'A1', 100), (22, 'A2', 200), (22, 'A3', 400), (93, 'A4', 500);  
-select * from IRAs; 
+describe iras; 
+insert into iras values(1, 'A1', 100), (22, 'A2', 200), (22, 'A3', 400), (93, 'A4', 500);  
+select * from iras; 
 show tables;
 
 
 
 
-drop table if exists AddressCity; 
-create table AddressCity(IDN int, CompanyCity varchar(22), HomeCity varchar(22) ); 
+drop table if exists addresscity; 
+create table addresscity(idn int, companycity varchar(22), homecity varchar(22) ); 
 /* Create the AddressCity table */ 
-describe AddressCity; 
-insert into AddressCity values (1, 'Saint Louis', 'Saint Louis'), (12, 'Saint Louis', 'Chesterfield'), (22, 'Chesterfield', 'Saint Louis'), (93, 'Chesterfield', 'Chesterfield'); 
-select * from AddressCity; 
+describe addresscity; 
+insert into addresscity values (1, 'Saint Louis', 'Saint Louis'), (12, 'Saint Louis', 'Chesterfield'), (22, 'Chesterfield', 'Saint Louis'), (93, 'Chesterfield', 'Chesterfield'); 
+select * from addresscity; 
 
 show tables;
 select * from employees;
 
 
-select e.LName,a.HomeCity,  sum(Balance),count(IRA_acct)
-from employees e, IRAs i, AddressCity a
-where e.IDN=i.IDN and i.IDN=a.IDN
-group by e.LName,a.HomeCity;
+select e.lname,a.homecity,  sum(balance),count(ira_acct)
+from employees e, iras i, addresscity a
+where e.idn=i.idn and i.idn=a.idn
+group by e.lname,a.homecity;
 
 
-select * from employees where LName like '%t_%';
+select * from employees where lname like '%t_%';
 
 drop table x;
-create table x (select LName from employees);
+create table x (select lname from employees);
 
 select * from x;
 
-select * from AddressCity where HomeCity='chesterfield';
+select * from addresscity where homecity='chesterfield';
 
 show tables;
 select * from employees;
-select * from IRAs;
+select * from iras;
 
-select LName,Salary from employees union select IRA_acct,Balance from IRAs;
+select lname,salary from employees union select ira_acct,balance from iras;
 
 #select LName,Salary,count(coalesce(IRA_acct,0)), sum(coalesce(Balance,0)) ira_saved 
 select LName, Salary,count(IRA_acct),sum(Balance) ira_saved
